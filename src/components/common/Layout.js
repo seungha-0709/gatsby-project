e30d93f1e5b5893180d1f384a1c859dcbef0b074
Seungha <seungha-0709@erpper.com>
2021-04-24 12:13:56 +0900

    CSS 수정

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -26,17 +26,21 @@ import '../../styles/app.css'
 * styles, and meta data for each page.
 *
 */
+
+
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
+
     const site = data.allGhostSettings.edges[0].node
     const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
     const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
     const [isMenuOpen, setIsMenuOpen] = useState(false)
-
     const [toggleShift, setToggleShift] = useState(false)
+    const [isDark, setIsDark] = useState(false)
 
     const handleToggleClick = () => {
         setToggleShift(!toggleShift)
+        setIsDark(!isDark)
     }
 
     const handleMenuOpen = () => {
@@ -51,7 +55,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <body className={bodyClass} />
             </Helmet>
 
-            <div className="viewport">
+            <div className={classNames('viewport', isDark ? 'darkmode' : '')}>
 
                 <div className="viewport-top">
                     {/* {isHome && } */}
@@ -60,15 +64,15 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                         <aside className="lnb-wrap" style={{ width: '25%' }}>
                             <div className='lnb-bg'></div>
                             <div className={classNames('lnb-nav', isMenuOpen ? 'open' : '')}>
-                                <span style={{ display: 'block' }}><IoIosArrowBack onClick={handleMenuOpen} style={{ color: '#fff', fontSize: '20px' }} /></span>
-                                <Navigation data={site.navigation} navClass="site-lnb-nav-item" />
+                                <span style={{ cursor: 'pointer' }}><IoIosArrowBack onClick={handleMenuOpen} style={{ color: '#fff', fontSize: '30px' }} /></span>
+                                <div className="site-lnb-nav"><Navigation data={site.navigation} navClass="site-lnb-nav-item" /></div>
                             </div>
                         </aside>
                         <div className="content-wrap" style={{ width: '75%' }}>
                             <header className="site-head">
                                 <div style={{ display: 'flex' }}>
-                                    <span className="toggle-circle">
-                                        <GiHamburgerMenu style={{ fontSize: '20px' }} onClick={handleMenuOpen} /></span>
+                                    <span className="btn-circle">
+                                        <GiHamburgerMenu style={{ fontSize: '20px', color: '#fff' }} onClick={handleMenuOpen} /></span>
                                     <div className="toggle-button">
                                         <div className="toggle-bg">
                                             <div
@@ -90,10 +94,9 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                 <footer className="site-foot">
                                     <div className="site-foot-nav container">
                                         <div className="site-foot-nav-left">
-                                            <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
-                                        </div>
-                                        <div className="site-foot-nav-right">
-                                            <Navigation data={site.navigation} navClass="site-foot-nav-item" />
+                                            {/* <Link to="/">{site.title}</Link> */}
+                                             © 2021 Published with <a style={{ display: 'inline-block' }} className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                                            <span style={{ display: 'block' }}>Designed by Seungha</span>
                                         </div>
                                     </div>
                                 </footer>
