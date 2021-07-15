44c6752b1c8a9a9aef79ee3addf06d7e54be7934
Seungha <sh.kim.x928@gmail.com>
2021-07-15 19:55:40 +0900

    siteConfig 수정

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `My Devlog - Daily Learning`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `우왕좌왕 개발 블로그. 배우고 생각하고 느끼는 것들을 적는 공간.`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `a Journal with, or for X `, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `just stories`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `My devlog`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `a Journal with, or for X`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
