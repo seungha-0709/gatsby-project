0852dc7c6d2d28b41e42b5e59aa5a6280d3f4fa7
Seungha <sh.kim.x928@gmail.com>
2021-09-13 19:52:41 +0900

    about 수정

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,6 +11,7 @@ const About = () => {
   const P = styled.p`
     font-family: 'Noto Serif CJK';
     font-weight: 300;
+    font-size: 0.9rem;
   `
   const SmallP = styled.p`
     font-family: 'Noto Serif CJK';
@@ -23,10 +24,7 @@ const About = () => {
       <div className="container" style={{ position: 'relative' }}>
 
         <P>김승하</P>
-        <P>철학과 윤리학에 관심이 많습니다.</P>
-        <P>어릴 적부터 이런 저런 사상들을 좋아했고 뼛속까지 본투비 문과라는 소리를 듣고 자라왔지만<br />
-          놀랍게도 직업은 웹 프런트엔드 개발자. 판교에서 일하고 있습니다.
-        </P>
+        <P>원래는 개발 공부 기록하려고 만든 블로그인데 어쩌다 일기장이 되어버린 웹 사이트.</P>
         <hr />
       </div>
     </Layout>
