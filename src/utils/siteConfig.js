07912f48c397bc24af0b82dd61b044d8ebcc6d50
Seungha <sh.kim.x928@gmail.com>
2021-09-23 12:46:10 +0900

    Modify page-about, siteConfig

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 6, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `a Journal with, or for X `, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `just stories`, // This allows an alternative site description for meta data for pages.
