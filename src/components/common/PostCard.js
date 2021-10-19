f7fe78d82f95fb401b24527fbd871bcaaa47a341
Seungha <sh.kim.x928@gmail.com>
2021-10-19 14:32:40 +0900

    layout modified

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -7,16 +7,16 @@ import styled from 'styled-components';
 import { isDarkShift, isToggle } from '../../store/dark'
 
 const PostCard = ({ post }) => {
-    const url = `/${post.slug}/`
+  const url = `/${post.slug}/`
 
-    const Container = styled.div`
+  const Container = styled.div`
       width: 100%;
       display: flex;
       flex-wrap: nowrap;
       margin: 20px 0;
     `
-    const Date = styled.div`
-      width: 80px;
+  const Date = styled.div`
+      width: 25%;
       margin-right: 20px;
       font-size: 0.8rem;
       text-align: right;
@@ -24,14 +24,14 @@ const PostCard = ({ post }) => {
           font-size: 0.6rem;
       }
     `
-    const PostBox = styled.div`
-      width: 100%;
+  const PostBox = styled.div`
+      width: 75%;
       padding-left: 20px;
       padding-bottom: 40px;
       border-left: 1px solid #ddd;
     `
 
-    const PostCardTags = styled.div`
+  const PostCardTags = styled.div`
       display: block;
       width: 100%;
       font-size: 0.8rem;
@@ -44,7 +44,7 @@ const PostCard = ({ post }) => {
           font-size: 0.6rem;
       }
     `
-    const PostCardTitle = styled.h2`
+  const PostCardTitle = styled.h2`
       display: block;
       width: 100%;
       font-weight: 600;
@@ -54,7 +54,7 @@ const PostCard = ({ post }) => {
           font-size: 1rem;
       }
     `
-    const PostCardExcerpt = styled.div`
+  const PostCardExcerpt = styled.div`
       display: block;
       width: 100%;
       font-size: 0.8rem;
@@ -71,7 +71,7 @@ const PostCard = ({ post }) => {
       }
     `
 
-    const PostCardExcerptMobile = styled.div`
+  const PostCardExcerptMobile = styled.div`
       display: none;
       
       @media screen and (max-width: 400px) {
@@ -89,48 +89,48 @@ const PostCard = ({ post }) => {
     `
 
 
-    return (
-        <Link to={url} className="none-underline">
-            <Container>
+  return (
+    <Link to={url} className="none-underline">
+      <Container>
 
-                <Date>
-                    {moment(post.published_at).format('MMMM.DD YYYY')}
-                </Date>
+        <Date>
+          {moment(post.published_at).format('MMMM.DD YYYY')}
+        </Date>
 
-                <PostBox>
-                    <header className="post-card-header">
-                        {post.tags && <PostCardTags> <Tags post={post} visibility="public" autolink={false} /></PostCardTags>}
-                        {post.featured && <span>Featured</span>}
-                        <PostCardTitle>{post.title}</PostCardTitle>
-                    </header>
-                    <PostCardExcerpt>{post.excerpt.substr(0, 243)}</PostCardExcerpt>
-                    <PostCardExcerptMobile>{post.excerpt.substr(0, 80)}</PostCardExcerptMobile>
+        <PostBox>
+          <header className="post-card-header">
+            {post.tags && <PostCardTags> <Tags post={post} visibility="public" autolink={false} /></PostCardTags>}
+            {post.featured && <span>Featured</span>}
+            <PostCardTitle>{post.title}</PostCardTitle>
+          </header>
+          <PostCardExcerpt>{post.excerpt.substr(0, 243)}</PostCardExcerpt>
+          <PostCardExcerptMobile>{post.excerpt.substr(0, 80)}</PostCardExcerptMobile>
 
-                </PostBox>
+        </PostBox>
 
-            </Container>
-        </Link>
+      </Container>
+    </Link>
 
-    )
+  )
 }
 
 PostCard.propTypes = {
-    post: PropTypes.shape({
-        slug: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        feature_image: PropTypes.string,
-        featured: PropTypes.bool,
-        tags: PropTypes.arrayOf(
-            PropTypes.shape({
-                name: PropTypes.string,
-            })
-        ),
-        excerpt: PropTypes.string.isRequired,
-        primary_author: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            profile_image: PropTypes.string,
-        }).isRequired,
+  post: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    feature_image: PropTypes.string,
+    featured: PropTypes.bool,
+    tags: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+      })
+    ),
+    excerpt: PropTypes.string.isRequired,
+    primary_author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      profile_image: PropTypes.string,
     }).isRequired,
+  }).isRequired,
 }
 
 export default PostCard
