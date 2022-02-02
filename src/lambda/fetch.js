3e26ebc0b5dd52d9ace750e61e2e1bc04f25d1cf
Seungha <sh.kim.x928@gmail.com>
2022-02-02 17:20:59 +0900

    modify limits

--- src/lambda/fetch.js
+++ src/lambda/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 exports.handler = function (event, context, callback) {
-  const apiRoot = "https://kimseungha.me/ghost/api/v3/content/posts/?key=a66ba36570db1489323eb9a32c&include=tags&limit=5"
+  const apiRoot = "https://kimseungha.me/ghost/api/v3/content/posts/?key=a66ba36570db1489323eb9a32c&include=tags&limit=all"
   const accessKey = 'a66ba36570db1489323eb9a32c'
 
   axios.get(apiRoot).then(res => {
